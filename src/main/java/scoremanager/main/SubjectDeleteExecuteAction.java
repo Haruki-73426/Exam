cf854bf46511710f36aa37d295e9c8296c4581aa
Haruki-73426 <tks2573426@stu.o-hara.ac.jp>
2026-04-28 13:55:52 +0900

    科目管理

--- src/main/java/scoremanager/main/SubjectDeleteExecuteAction.java
+++ src/main/java/scoremanager/main/SubjectDeleteExecuteAction.java
@@ -1,5 +1,31 @@
 package scoremanager.main;
 
-public class SubjectDeleteExecuteAction {
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
+import bean.Subject;
+import bean.Teacher;
+import dao.SubjectDao;
+import tool.Action;
+
+public class SubjectDeleteExecuteAction extends Action {
+
+  @Override
+  public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+
+    HttpSession session = req.getSession();
+    Teacher teacher = (Teacher) session.getAttribute("user");
+
+    String cd = req.getParameter("cd");
+
+    SubjectDao subjectDao = new SubjectDao();
+    Subject subject = subjectDao.get(cd, teacher.getSchool());
+
+    if (subject != null) {
+      subjectDao.delete(subject);
+    }
+
+    req.getRequestDispatcher("subject_delete_done.jsp").forward(req, res);
+  }
 }
