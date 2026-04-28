9b25a23399077a2675ce486e18130f0ff318fa77
Haruki-73426 <tks2573426@stu.o-hara.ac.jp>
2026-04-28 14:49:51 +0900

    科目管理

--- src/main/java/scoremanager/main/SubjectCreateAction.java
+++ src/main/java/scoremanager/main/SubjectCreateAction.java
@@ -1,11 +1,7 @@
 package scoremanager.main;
 
-import bean.Subject;
-import bean.Teacher;
-import dao.SubjectDao;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 import tool.Action;
 
 public class SubjectCreateAction extends Action {
@@ -13,22 +9,7 @@ public class SubjectCreateAction extends Action {
     @Override
     public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
 
-        HttpSession session = req.getSession();
-        Teacher teacher = (Teacher) session.getAttribute("user");
+        req.getRequestDispatcher("subject_create.jsp").forward(req, res);
 
-        String cd = req.getParameter("cd");
-
-        SubjectDao subjectDao = new SubjectDao();
-        Subject subject = subjectDao.get(cd, teacher.getSchool());
-
-        if (subject == null) {
-            req.getRequestDispatcher("SubjectList.action").forward(req, res);
-            return;
-        }
-
-        req.setAttribute("cd", subject.getCd());
-        req.setAttribute("name", subject.getName());
-
-        req.getRequestDispatcher("subject_update.jsp").forward(req, res);
     }
 }
